a1bad81fadc16866a7778ea85d9cf3906035324c
Alexander mena <alexismena-90@hotmail.com>
2025-01-27 19:36:44 -0800

    readme

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Define the Song schema for embedded documents
 const songSchema = new Schema(
@@ -16,10 +16,6 @@ const songSchema = new Schema(
       type: String,
       required: true,
     },
-<<<<<<< HEAD
-  ]
-});
-=======
     album: {
       type: String,
     },
@@ -34,7 +30,6 @@ const songSchema = new Schema(
     _id: false,
   }
 );
->>>>>>> 1167ebfc0f463441e78f426111556c28cecbda49
 
 // Define the User schema
 const userSchema = new Schema({
@@ -48,7 +43,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match a valid email address!'],
+    match: [/.+@.+\..+/, "Must match a valid email address!"],
   },
   password: {
     type: String,
@@ -59,8 +54,8 @@ const userSchema = new Schema({
 });
 
 // Pre-save middleware to hash passwords
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -72,6 +67,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
